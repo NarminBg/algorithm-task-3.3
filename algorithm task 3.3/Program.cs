04361621204f6ab9dbc092cc53c6f7f15ad44b69
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-29 19:03:33 +0400

    Add project files.

---
+++ algorithm task 3.3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace algorithm_task_3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number = decimal.Parse(Console.ReadLine());
+            decimal k, tam;
+            tam = (int)number;
+            k = (number - tam) * 1000;
+
+            tam /= 1000;
+
+            k = k + tam;
+
+            Console.WriteLine($"{k}");
+        }
+    }
+}
